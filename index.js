9c6f6429d801a52bea623f2f03c2e7392efacd50
Rajganez <ganezsouth@live.com>
2025-05-19 10:54:18 +0530

    Altered frontend URL because of the cors deployment error message

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://blogappbyraj.netlify.app/",
+    origin: "https://blogappbyraj.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
